f8ce87900a14c459e6414a5ba7269374d32fc821
amssj <69820922+amssj@users.noreply.github.com>
2020-08-21 17:30:19 +0200

    In_out_update

--- IO/api.py
+++
@@ -1,6 +0,0 @@
-from urllib.request import urlopen
-import json
-from pprint import pprint
-antwoord=urlopen('https://www.googleapis.com/books/v1/volumes?q=inauthor:"Niklas+Luhmann"').read()
-data=json.loads(antwoord.decode("utf-8"))
-pprint(data)
